b5f08beb4077336a920912098e2b65180814ef2c
Supriyo <supriyosarkar282@gmail.com>
2020-08-09 00:23:35 +0530

    made the colBox

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,23 @@ const App: React.FC = () => {
     }
     return rows;
   });
-  console.log(grid);
-  return <div className="App">yo</div>;
+  return (
+    <div>
+      {grid.map((rows, i) =>
+        rows.map((col, k) => (
+          <div
+            key={`${i}-${k}`}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: grid[i][k] ? "red" : undefined,
+              border: "solid 1px black",
+            }}
+          />
+        ))
+      )}
+    </div>
+  );
 };
 
 export default App;

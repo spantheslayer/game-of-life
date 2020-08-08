075d6193f9dfaf71a682fb15d586c2d00a97e80a
Supriyo <supriyosarkar282@gmail.com>
2020-08-09 00:32:33 +0530

    added event func

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import produce from "immer";
 
 const numRows = 50;
 const numCols = 50;
@@ -22,6 +23,12 @@ const App: React.FC = () => {
         rows.map((col, k) => (
           <div
             key={`${i}-${k}`}
+            onClick={() => {
+              const newGrid = produce(grid, (gridCopy) => {
+                gridCopy[i][k] = 1;
+              });
+              setGrid(newGrid);
+            }}
             style={{
               width: 20,
               height: 20,

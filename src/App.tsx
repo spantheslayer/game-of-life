cb9fa7eb42ccf3d6cf91b737c530842abd65bf50
Supriyo <supriyosarkar282@gmail.com>
2020-08-09 00:12:07 +0530

    created the fGrid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+
+const numRows = 50;
+const numCols = 50;
 
 const App: React.FC = () => {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  });
+  console.log(grid);
   return <div className="App">yo</div>;
 };
 
